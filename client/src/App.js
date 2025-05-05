1dfdc92a7719cda498d131cd02535372d20befaa
Abdulmuid1 <olaniyanabdulmuid@gmail.com>
2025-05-04 22:23:18 -0600

    Integrated a React based Frontend
    Integrated buttons for sms and speaker to display delayed messages
    
    Implemented a user-friendly webpage for the WTAS app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { FaSms, FaBullhorn } from "react-icons/fa";
+import "./App.css";
+import DelayAlerts from "./Delay_alerts";  // render the DelayAlerts component
+import SpeakerAnnouncements from "./Speaker_announce"; // render the Speaker component
+import SmsAlerts from "./Sms_alerts"; // render the Sms component
 
 function App() {
+  const [showSMS, setShowSMS] = useState(false);
+  const [showSpeaker, setShowSpeaker] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Winter Transit Alert System (WTAS)</h1>
+        <p>Stay updated on real-time delays for buses and trains.</p>
       </header>
+
+      <DelayAlerts />
+
+      <div style={{ margin: "20px" }}>
+      <button className="attention-button" onClick={() => setShowSMS(!showSMS)}>
+        <FaSms /> Send SMS Alerts
+       </button>
+      <button className="attention-button" onClick={() => setShowSpeaker(!showSpeaker)}>
+        <FaBullhorn /> Announce via Speaker
+      </button>
+     </div>
+
+
+
+      {showSMS && <SmsAlerts />}
+      {showSpeaker && <SpeakerAnnouncements />}
+
+      <footer>
+      <p>&copy; {new Date().getFullYear()} Winter Transit Alert System - Edmonton</p>
+     </footer>
+     
     </div>
+    
   );
 }
 
